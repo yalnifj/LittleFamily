0c3bc5becae4c4cf5f86d50a850f5c7bd86bcaa9
yalnifj <yalnifj@gmail.com>
2014-12-29 11:22:11 -0700

    FamilySearch certification requirements

--- app/src/main/java/org/finlayfamily/littlefamily/activities/FSLoginActivity.java
+++ app/src/main/java/org/finlayfamily/littlefamily/activities/FSLoginActivity.java
@@ -35,14 +35,6 @@ import org.finlayfamily.littlefamily.R;
  * A login screen that offers login via email/password.
  */
 public class FSLoginActivity extends Activity implements LoaderCallbacks<Cursor> {
-
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
@@ -146,13 +138,11 @@ public class FSLoginActivity extends Activity implements LoaderCallbacks<Cursor>
     }
 
     private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
-        return email.contains("@");
+        return email.length() > 1;
     }
 
     private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
-        return password.length() > 4;
+        return password.length() > 1;
     }
 
     /**
@@ -261,24 +251,7 @@ public class FSLoginActivity extends Activity implements LoaderCallbacks<Cursor>
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
 
-            try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-            // TODO: register the new account here.
             return true;
         }
 
