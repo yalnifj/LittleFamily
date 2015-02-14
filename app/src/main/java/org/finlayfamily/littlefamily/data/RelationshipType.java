e3074fe66d30090b90af335f23115eaaff09b613
john finlay <yalnifj@gmail.com>
2015-02-13 17:44:54 -0700

    more database support

---
+++ app/src/main/java/org/finlayfamily/littlefamily/data/RelationshipType.java
@@ -0,0 +1,19 @@
+package org.finlayfamily.littlefamily.data;
+import org.gedcomx.types.*;
+
+public enum RelationshipType
+{
+	PARENTCHILD(0), SPOUSE(1);
+	
+	int id;
+	public RelationshipType(int id) {
+		this.id = id;
+	}
+	public int getId() {
+		return id;
+	}
+	
+	public static RelationshipType getTypeFromId(int id) {
+		return values()[id];
+	}
+}
--- app/src/main/java/org/finlayfamily/littlefamily/db/DBHelper.java
+++ app/src/main/java/org/finlayfamily/littlefamily/db/DBHelper.java
@@ -4,15 +4,21 @@ import android.content.*;
 import android.database.*;
 import android.database.sqlite.*;
 import android.util.*;
+import com.google.android.gms.internal.*;
 import java.util.*;
 import org.finlayfamily.littlefamily.data.*;
 import org.gedcomx.types.*;
+
+import org.finlayfamily.littlefamily.data.RelationshipType;
 
 public class DBHelper extends SQLiteOpenHelper {
 	private static final int DATABASE_VERSION = 1;
 	private static final String DATABASE_NAME = "LittleFamily.db";
 
 	private static final String TABLE_LITTLE_PERSON = "littleperson";
+	private static final String TABLE_RELATIONSHIP = "relationship";
+	private static final String TABLE_MEDIA = "media";
+	private static final String TABLE_TAGS = "tags";
 	
 	private static final String COL_ID = "id";
 	private static final String COL_NAME = "name";
@@ -22,12 +28,21 @@ public class DBHelper extends SQLiteOpenHelper {
 	private static final String COL_BIRTH_DATE = "birthDate";
 	private static final String COL_AGE = "age";
 	private static final String COL_GENDER = "gender";
+	private static final String COL_ID1 = "id1";
+	private static final String COL_ID2 = "id2";
+	private static final String COL_TYPE = "type";
+	private static final String COL_LOCAL_PATH = "localpath";
 	
 	private static final String CREATE_LITTLE_PERSON = "create table "+TABLE_LITTLE_PERSON+" ( " +
 			" "+COL_ID+" integer primary key, "+COL_GIVEN_NAME+" text, "+COL_NAME+" text, " +
 			" "+COL_FAMILY_SEARCH_ID+" text, "+COL_PHOTO_PATH+" text, "+COL_BIRTH_DATE+" integer, "
 			+COL_AGE+" integer, "+COL_GENDER+" char );";
-	
+	private static final String CREATE_RELATIONSHIP = "create table " + TABLE_RELATIONSHIP + " ( " 
+			+COL_ID1+" integer, "+COL_ID2+" integer, "+COL_TYPE+" integer );";
+	private static final String CREATE_MEDIA = "create table "+TABLE_MEDIA+" ( "
+			+COL_ID+" integer primary key, "+COL_FAMILY_SEARCH_ID+" text, "
+			+COL_TYPE+" integer, "+COL_LOCAL_PATH+" text )";
+			
 	private Context context;
 
 	public DBHelper(Context context) {
@@ -38,7 +53,8 @@ public class DBHelper extends SQLiteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL(CREATE_LITTLE_PERSON);
-		
+		db.execSQL(CREATE_RELATIONSHIP);
+		db.execSQL(CREATE_MEDIA);
 	}
 	
 	@Override
@@ -121,6 +137,34 @@ public class DBHelper extends SQLiteOpenHelper {
 		return person;
 	}
 	
+	public List<LittlePerson> getRelativesForPerson(int id) {
+		List<LittlePerson> people = new ArrayList<>();
+		SQLiteDatabase db = getReadableDatabase();
+		String[] projection = {
+			COL_GIVEN_NAME, COL_GENDER, COL_PHOTO_PATH, COL_NAME,
+			COL_NAME, COL_AGE, COL_BIRTH_DATE, COL_FAMILY_SEARCH_ID, 
+			COL_ID
+		};
+		String selection = "r."+COL_ID1 + " LIKE ? or r."+COL_ID2+" LIKE ?";
+
+		String[] selectionArgs = { String.valueOf(id), String.valueOf(id) };
+		String tables = TABLE_LITTLE_PERSON + " p join " + TABLE_RELATIONSHIP + " r on r."+COL_ID1+"=p."+COL_ID
+					+" or r."+COL_ID2+"=p."+COL_ID;
+
+		Map<Long, LittlePerson> personMap = new HashMap<>();
+		Cursor c = db.query(tables, projection, selection, selectionArgs, null, null, COL_ID);
+		while (c.moveToNext()) {
+			LittlePerson p = personFromCursor(c);
+			personMap.put(p.getId(), p);
+		}
+
+		c.close();
+		db.close();
+		people.addAll(personMap.values());
+
+		return people;
+	}
+	
 	public LittlePerson personFromCursor(Cursor c) {
 		LittlePerson person = new LittlePerson();
 		person.setAge(c.getInt( c.getColumnIndexOrThrow(COL_AGE) ));
@@ -148,5 +192,82 @@ public class DBHelper extends SQLiteOpenHelper {
 		person.setName(c.getString(c.getColumnIndexOrThrow(COL_NAME)));
 		person.setPhotoPath(c.getString(c.getColumnIndexOrThrow(COL_PHOTO_PATH)));
 		return person;
+	}
+	
+	public long persistRelationship(Relationship r) {
+		// dont allow duplicate relationships
+		if (this.getRelationship(r.getId1(), r.getId2(), r.getType()) != null)
+			return -1;
+		
+		SQLiteDatabase db = getWritableDatabase();
+
+		ContentValues values = new ContentValues();
+		values.put(COL_ID1, r.getId1());
+		values.put(COL_ID2, r.getId2());
+		values.put(COL_TYPE, r.getType().getId());
+		
+		// -- add
+		long rowid = db.insert(TABLE_LITTLE_PERSON, null, values);
+			
+		db.close();
+		
+		return rowid;
+	}
+	
+	public Relationship getRelationship(int id1, int id2, RelationshipType type) {
+		SQLiteDatabase db = getReadableDatabase();
+		String[] projection = {
+			COL_ID1, COL_ID2, COL_TYPE
+		};
+		String selection = "(("+COL_ID1 + " LIKE ? and "+COL_ID2+" LIKE ?) "
+				+" OR ("+COL_ID1 + " LIKE ? and "+COL_ID2+" LIKE ?)) "
+				+" AND "+COL_TYPE+" LIKE ?";
+		String[] selectionArgs = { String.valueOf(id1), String.valueOf(id2), 
+				String.valueOf(id2), String.valueOf(id1), String.valueOf(type.getId()) };
+		String tables = TABLE_RELATIONSHIP;
+
+		Relationship r = null;
+		Cursor c = db.query(tables, projection, selection, selectionArgs, null, null, COL_ID);
+		while (c.moveToNext()) {
+			r = relationshipFromCursor(c);
+		}
+
+		c.close();
+		db.close();
+
+		return r;
+	}
+	
+	public List<Relationship> getRelationshipsForPerson(int id) {
+		List<Relationship> relationships = new ArrayList<>();
+		SQLiteDatabase db = getReadableDatabase();
+		String[] projection = {
+			COL_ID1, COL_ID2, COL_TYPE
+		};
+		String selection = COL_ID1 + " LIKE ? or "+COL_ID2+" LIKE ?";
+			
+		String[] selectionArgs = { String.valueOf(id), String.valueOf(id) };
+		String tables = TABLE_RELATIONSHIP;
+
+		Cursor c = db.query(tables, projection, selection, selectionArgs, null, null, COL_ID);
+		while (c.moveToNext()) {
+			Relationship r = relationshipFromCursor(c);
+			relationships.add(r);
+		}
+
+		c.close();
+		db.close();
+		
+		return relationships;
+	}
+	
+	public Relationship relationshipFromCursor(Cursor c) {
+		Relationship r = new Relationship();
+		r.setId1(c.getInt(c.getColumnIndexOrThrow(COL_ID1)));
+		r.setId2(c.getInt(c.getColumnIndexOrThrow(COL_ID2)));
+		int type = c.getInt(c.getColumnIndexOrThrow(COL_TYPE));
+		RelationshipType rt = RelationshipType.getTypeFromId(type);
+		r.setType(rt);
+		return r;
 	}
 }
