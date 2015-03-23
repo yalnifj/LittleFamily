31b35a5f61d6d66e2af7709e993cbe1b62c583a6
yalnifj <yalnifj@gmail.com>
2015-03-23 16:51:32 -0600

    Improved performance of coloring book

--- LittleFamilyApp/src/main/java/org/finlayfamily/littlefamily/views/ColoringView.java
+++ LittleFamilyApp/src/main/java/org/finlayfamily/littlefamily/views/ColoringView.java
@@ -8,12 +8,21 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.ImageView;
 
+import com.jabistudio.androidjhlabs.filter.EdgeFilter;
+import com.jabistudio.androidjhlabs.filter.GrayscaleFilter;
+import com.jabistudio.androidjhlabs.filter.InvertFilter;
 import com.jabistudio.androidjhlabs.filter.LaplaceFilter;
+import com.jabistudio.androidjhlabs.filter.SmartBlurFilter;
 import com.jabistudio.androidjhlabs.filter.util.AndroidUtils;
+
+import org.finlayfamily.littlefamily.R;
+import org.finlayfamily.littlefamily.activities.tasks.ColoringImageFilterTask;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,19 +30,23 @@ import java.util.List;
 /**
  * Created by jfinlay on 1/22/2015.
  */
-public class ColoringView extends ImageView {
+public class ColoringView extends ImageView implements ColoringImageFilterTask.Listener{
     public int width;
     public  int height;
+    private Bitmap originalBitmap;
     private Bitmap outlineBitmap;
     private Bitmap  mBitmap;
     private Canvas  mCanvas;
     private Path    mPath;
     private Paint   mBitmapPaint;
+    private Paint   paint2;
     private Context context;
     private Paint circlePaint;
     private Path circlePath;
     private Paint       mPaint;
-    private LaplaceFilter filter;
+    private boolean loaded;
+    private boolean complete;
+    private Drawable spinner;
 
     public ColoringView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -57,9 +70,10 @@ public class ColoringView extends ImageView {
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setStrokeJoin(Paint.Join.ROUND);
         mPaint.setStrokeCap(Paint.Cap.ROUND);
-        mPaint.setStrokeWidth(12);
+        mPaint.setStrokeWidth(10);
+        paint2 = new Paint(Paint.DITHER_FLAG);
 
-        filter = new LaplaceFilter();
+        spinner = context.getResources().getDrawable(R.drawable.abc_spinner_mtrl_am_alpha);
     }
 
     @Override
@@ -73,6 +87,9 @@ public class ColoringView extends ImageView {
         background.setColor(Color.WHITE);
         mCanvas.drawRect(0,0,w,h,background);
 
+        if (originalBitmap!=null)
+            originalBitmap = Bitmap.createBitmap(originalBitmap, 0, 0, w, h);
+
         if (outlineBitmap!=null)
             outlineBitmap = Bitmap.createBitmap(outlineBitmap, 0, 0, w, h);
     }
@@ -80,45 +97,57 @@ public class ColoringView extends ImageView {
     @Override
     protected void onDraw(Canvas canvas)
     {
-        super.onDraw(canvas);
+        //super.onDraw(canvas);
+        if (!loaded) {
+            spinner.draw(canvas);
+        } else {
+            canvas.drawBitmap(originalBitmap, 0, 0, paint2);
+            canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
+            if (!complete) {
+                if (outlineBitmap != null) {
+                    canvas.drawBitmap(outlineBitmap, 0, 0, paint2);
+                }
 
-        canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
-        canvas.drawBitmap(outlineBitmap, 0, 0, mBitmapPaint);
-
-        //canvas.drawPath(mPath, mPaint);
-
-        canvas.drawPath(circlePath, circlePaint);
-
+                canvas.drawPath(circlePath, circlePaint);
+            }
+        }
     }
 
     @Override
     public void setImageBitmap(Bitmap bm)
     {
-        super.setImageBitmap(bm);
-        int w = this.getWidth();
-        int h = this.getHeight();
-        mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-        mCanvas = new Canvas(mBitmap);
-        mPaint.setStrokeWidth(w<h?w*0.15f:h*0.15f);
-        Paint background = new Paint();
-        background.setColor(Color.WHITE);
-        mCanvas.drawRect(0,0,w,h,background);
+        originalBitmap = bm;
 
-        int[] src = AndroidUtils.bitmapToIntArray(bm);
-        filter.filter(src, w, h);
-        outlineBitmap = Bitmap.createBitmap(src, w, h, Bitmap.Config.ARGB_8888);
+        loaded = false;
+        complete = false;
+
+        if (bm!=null) {
+            int w = this.getWidth();
+            int h = this.getHeight();
+            mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            mCanvas = new Canvas(mBitmap);
+            mPaint.setStrokeWidth(w < h ? w * 0.15f : h * 0.15f);
+            Paint background = new Paint();
+            background.setColor(Color.WHITE);
+            mCanvas.drawRect(0, 0, w, h, background);
+
+            ColoringImageFilterTask task = new ColoringImageFilterTask(this);
+            task.execute(originalBitmap);
+        }
     }
 
     private float mX, mY;
     private static final float TOUCH_TOLERANCE = 4;
 
     private void touch_start(float x, float y) {
+        if (!loaded) return;
         mPath.reset();
         mPath.moveTo(x, y);
         mX = x;
         mY = y;
     }
     private void touch_move(float x, float y) {
+        if (!loaded) return;
         float dx = Math.abs(x - mX);
         float dy = Math.abs(y - mY);
         if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
@@ -130,11 +159,12 @@ public class ColoringView extends ImageView {
             mCanvas.drawPath(mPath,  mPaint);
 
             circlePath.reset();
-            circlePath.addCircle(mX, mY, 40, Path.Direction.CW);
+            circlePath.addCircle(mX, mY, 35, Path.Direction.CW);
         }
     }
 
     private void touch_up() {
+        if (!loaded) return;
         mPath.lineTo(mX, mY);
         circlePath.reset();
         // commit the path to our offscreen
@@ -155,6 +185,7 @@ public class ColoringView extends ImageView {
             }
         }
         if (count > total * 0.90) {
+            complete = true;
             for(ColoringCompleteListener l : listeners) {
                 l.onColoringComplete();
             }
@@ -163,6 +194,7 @@ public class ColoringView extends ImageView {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        if (!loaded) return false;
         float x = event.getX();
         float y = event.getY();
 
@@ -183,10 +215,18 @@ public class ColoringView extends ImageView {
         return true;
     }
 
+    @Override
+    public void onComplete(Bitmap result) {
+        this.outlineBitmap = result;
+        loaded = true;
+        this.invalidate();
+    }
+
     private List<ColoringCompleteListener> listeners = new ArrayList<>();
     public void registerListener(ColoringCompleteListener l) {
         listeners.add(l);
     }
+
     public interface ColoringCompleteListener {
         public void onColoringComplete();
     }
