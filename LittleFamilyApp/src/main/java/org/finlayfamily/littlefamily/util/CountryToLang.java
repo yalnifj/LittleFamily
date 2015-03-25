89daf5610c7ec25aa18267896dff92484c5e7258
yalnifj <yalnifj@gmail.com>
2015-03-24 20:03:06 -0600

    Some place utilities to help with heritage game

---
+++ LittleFamilyApp/src/main/java/org/finlayfamily/littlefamily/util/CountryToLang.java
@@ -0,0 +1,20 @@
+package org.finlayfamily.littlefamily.util;
+
+import java.util.Locale;
+
+public class CountryToLang {
+	public static String getCountryLanguage(String country) {
+		for(Locale locale : Locale.getAvailableLocales()) {
+			if (locale.getDisplayCountry().equalsIgnoreCase(country)) {
+				return locale.getDisplayLanguage();
+			}
+		}
+		return "Other";
+	}
+	
+	public static String getTopPlace(String place) {
+		if (place==null) return null;
+		String[] parts = place.split("[, ]+");
+		return parts[parts.length-1];
+	}
+}
