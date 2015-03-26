768bed6a7404a0e2ad8339c5f665d6cf0470dbcd
john finlay <yalnifj@gmail.com>
2015-03-25 21:07:25 -0600

    Activity use heritage calc task

--- LittleFamilyApp/src/main/java/org/finlayfamily/littlefamily/activities/HeritageDressUpActivity.java
+++ LittleFamilyApp/src/main/java/org/finlayfamily/littlefamily/activities/HeritageDressUpActivity.java
@@ -8,10 +8,15 @@ import android.view.MenuItem;
 
 import org.finlayfamily.littlefamily.R;
 import org.finlayfamily.littlefamily.data.LittlePerson;
+import org.finlayfamily.littlefamily.activities.tasks.HeritageCalculatorTask;
+import java.util.*;
+import org.finlayfamily.littlefamily.data.*;
+import org.finlayfamily.littlefamily.util.*;
 
-public class HeritageDressUpActivity extends ActionBarActivity {
+public class HeritageDressUpActivity extends ActionBarActivity implements HeritageCalculatorTask.Listener {
 
     private LittlePerson person;
+	private SortedMap<String, Double> cultures;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,4 +26,30 @@ public class HeritageDressUpActivity extends ActionBarActivity {
         Intent intent = getIntent();
         person = (LittlePerson) intent.getSerializableExtra(ChooseFamilyMember.SELECTED_PERSON);
     }
+
+	@Override
+	public void onComplete(ArrayList<HeritagePath> paths)
+	{
+		ValueComparator vc = new ValueComparator();
+		cultures = new TreeMap<>(vc);
+		
+		Double total = 0.0;
+		for(HeritagePath path : paths) {
+			String place = path.getPlace();
+			total += path.getPercent();
+			if (cultures.get(place)==null) {
+				cultures.put(place, path.getPercent());
+			} else {
+				cultures.put(place, cultures.get(place) + path.getPercent());
+			}
+		}
+		if (total < 1.0) {
+			if (cultures.get("Unknown")==null) {
+				cultures.put("Unknown", 1.0 - total);
+			} else {
+				cultures.put("Unknown", cultures.get("Unknown") + 1.0 - total);
+			}
+		}
+	}
+
 }
