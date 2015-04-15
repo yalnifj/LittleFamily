02e81419826665176fb2bba6be5381aecf0fdcc6
john finlay <yalnifj@gmail.com>
2015-04-14 22:55:54 -0600

    adding speech to culture choice activity

--- LittleFamilyApp/src/main/java/org/finlayfamily/littlefamily/activities/ChooseCultureActivity.java
+++ LittleFamilyApp/src/main/java/org/finlayfamily/littlefamily/activities/ChooseCultureActivity.java
@@ -1,27 +1,20 @@
 package org.finlayfamily.littlefamily.activities;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.media.Image;
-import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.app.*;
+import android.content.*;
+import android.graphics.*;
+import android.os.*;
+import android.speech.tts.*;
+import android.widget.*;
+import java.util.*;
+import org.finlayfamily.littlefamily.*;
+import org.finlayfamily.littlefamily.activities.tasks.*;
+import org.finlayfamily.littlefamily.data.*;
+import org.finlayfamily.littlefamily.util.*;
+import org.finlayfamily.littlefamily.views.*;
+import org.gedcomx.types.*;
 
-import org.finlayfamily.littlefamily.R;
-import org.finlayfamily.littlefamily.activities.tasks.HeritageCalculatorTask;
-import org.finlayfamily.littlefamily.data.HeritagePath;
-import org.finlayfamily.littlefamily.data.LittlePerson;
-import org.finlayfamily.littlefamily.util.ImageHelper;
-import org.finlayfamily.littlefamily.views.PersonHeritageChartView;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-public class ChooseCultureActivity extends Activity implements HeritageCalculatorTask.Listener {
+public class ChooseCultureActivity extends Activity implements HeritageCalculatorTask.Listener, TextToSpeech.OnInitListener {
 
     private LittlePerson person;
     private Map<String, HeritagePath> cultures;
@@ -32,6 +25,8 @@ public class ChooseCultureActivity extends Activity implements HeritageCalculato
     private ImageView portraitImage;
     private ImageView dollImage;
     private HeritagePath selectedPath;
+	
+	private TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +47,8 @@ public class ChooseCultureActivity extends Activity implements HeritageCalculato
         Intent intent = getIntent();
         person = (LittlePerson) intent.getSerializableExtra(ChooseFamilyMember.SELECTED_PERSON);
         chartView.setPerson(person);
+		
+		tts = new TextToSpeech(this, this);
 
         HeritageCalculatorTask task = new HeritageCalculatorTask(this, this);
         task.execute(person);
@@ -68,14 +65,34 @@ public class ChooseCultureActivity extends Activity implements HeritageCalculato
             bm = ImageHelper.loadBitmapFromResource(this, relative.getDefaultPhotoResource(), 0, this.portraitImage.getWidth(), this.portraitImage.getHeight());
         }
         this.portraitImage.setImageBitmap(bm);
+		
+		String relationship = "";
+		int greats = selectedPath.getTreePath().size() - 2;
+		for(int g=0; g<greats; g++) {
+			relationship = relationship + getResources().getString(R.string.great)+", ";
+		}
+		if (selectedPath.getTreePath().size()>1) {
+			relationship = relationship + getResources().getString(R.string.grand)+", ";
+		}
+		if (relative.getGender()==GenderType.Female) {
+			getResources().getString(R.string.mother);
+		} else {
+			getResources().getString(R.string.father);
+		}
 
         this.cultureNameView.setText(selectedPath.getPlace());
+		String text = String.format(getResources().getString(R.string.you_are_percent),
+				selectedPath.getPercent()*100, selectedPath.getPlace(),
+				relationship, relative.getName());
+		speak(text);
     }
 
     @Override
     public void onComplete(ArrayList<HeritagePath> paths)
     {
         cultures = new HashMap<String, HeritagePath>();
+		titleView.setText(R.string.choose_culture);
+		speak(getResources().getString(R.string.choose_culture));
 
         for(HeritagePath path : paths) {
             String place = path.getPlace();
@@ -99,6 +116,38 @@ public class ChooseCultureActivity extends Activity implements HeritageCalculato
         if (uniquepaths.size()>0) {
             setSelectedPath(uniquepaths.get(0));
         }
-        titleView.setText(R.string.choose_culture);
+        
     }
+	
+	@Override
+    public void onInit(int code) {
+        if (code == TextToSpeech.SUCCESS) {
+            tts.setLanguage(Locale.getDefault());
+            tts.setSpeechRate(0.5f);
+			speak(getResources().getString(R.string.calculating_heritage));
+        } else {
+            tts = null;
+            //Toast.makeText(this, "Failed to initialize TTS engine.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (tts!=null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }
+	
+	private void speak(String message) {
+		if (tts!=null) {
+			if (Build.VERSION.SDK_INT > 20) {
+				tts.speak(message, TextToSpeech.QUEUE_FLUSH, null, null);
+			}
+			else {
+				tts.speak(message, TextToSpeech.QUEUE_FLUSH, null);
+			}
+		}
+	}
 }
