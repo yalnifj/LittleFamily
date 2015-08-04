b8f44708c7a39434fe474f29f8a49e119c1664d7
john finlay <yalnifj@gmail.com>
2015-08-03 18:18:02 -0600

    better person management

--- LittleFamilyApp/src/main/java/com/yellowforktech/littlefamilytree/activities/PersonDetailsDialog.java
+++ LittleFamilyApp/src/main/java/com/yellowforktech/littlefamilytree/activities/PersonDetailsDialog.java
@@ -8,9 +8,16 @@ import android.view.ViewGroup;
 
 import com.yellowforktech.littlefamilytree.R;
 import com.yellowforktech.littlefamilytree.data.LittlePerson;
+import android.graphics.Bitmap;
+import com.yellowforktech.littlefamilytree.util.ImageHelper;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.CheckBox;
+import java.text.SimpleDateFormat;
 
 public class PersonDetailsDialog extends DialogFragment {
     private LittlePerson person;
+	private SimpleDateFormat df = new SimpleDateFormat("YYYY");
 
     public PersonDetailsDialog(LittlePerson person) {
         this.person = person;
@@ -20,7 +27,35 @@ public class PersonDetailsDialog extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_person_details, container, false);
+        View view = inflater.inflate(R.layout.fragment_person_details, container, false);
+		ImageView portrait = (ImageView) view.findViewById(R.id.portraitImage);
+		if (person.getPhotoPath() != null) {
+			Bitmap bm = ImageHelper.loadBitmapFromFile(person.getPhotoPath(), ImageHelper.getOrientation(person.getPhotoPath()), portrait.getWidth(), portrait.getHeight(), false);
+			portrait.setImageBitmap(bm);
+		} else {
+			Bitmap bm = ImageHelper.loadBitmapFromResource(this.getActivity(), person.getDefaultPhotoResource(), 0, portrait.getWidth(), portrait.getHeight());
+			portrait.setImageBitmap(bm);
+		}
+		
+		TextView nameView = (TextView) view.findViewById(R.id.personName);
+		nameView.setText(person.getName());
+		
+		TextView remoteIdView = (TextView) view.findViewById(R.id.remoteId);
+		remoteIdView.setText(person.getFamilySearchId());
+		
+		TextView genderView = (TextView) view.findViewById(R.id.txtGender);
+		genderView.setText(person.getGender().toString());
+		
+		CheckBox activeBox = (CheckBox) view.findViewById(R.id.chkActive);
+		activeBox.setChecked(person.isActive());
+		
+		TextView birthDateView = (TextView) view.findViewById(R.id.txtBirthDate);
+		birthDateView.setText(df.format(person.getBirthDate()));
+		
+		TextView birthPlaceView = (TextView) view.findViewById(R.id.txtBirthPlace);
+		birthPlaceView.setText(person.getBirthPlace());
+		
+		return view;
     }
 
 }
