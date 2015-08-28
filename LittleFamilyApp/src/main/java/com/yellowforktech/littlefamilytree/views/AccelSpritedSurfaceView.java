b23c342395ca4ad93c115534255efcdc9e2f03d2
john finlay <yalnifj@gmail.com>
2015-08-28 08:47:31 -0600

    Accelerometer surface view

---
+++ LittleFamilyApp/src/main/java/com/yellowforktech/littlefamilytree/views/AccelSpritedSurfaceView.java
@@ -0,0 +1,18 @@
+package com.yellowforktech.littlefamilytree.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public abstract class AccelSpritedSurfaceView extends SpritedSurfaceView
+{
+	
+	public AccelSpritedSurfaceView(Context context) {
+        super(context);
+    }
+
+    public AccelSpritedSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    
+}
