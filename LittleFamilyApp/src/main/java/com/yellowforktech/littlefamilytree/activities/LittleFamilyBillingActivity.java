b33da580db2a60ca1013ae19e0b4450d84e94a83
jfinlay <yalnifj@gmail.com>
2016-09-02 14:17:57 -0600

    support Amazon purchases

--- LittleFamilyApp/src/main/java/com/yellowforktech/littlefamilytree/activities/LittleFamilyBillingActivity.java
+++ LittleFamilyApp/src/main/java/com/yellowforktech/littlefamilytree/activities/LittleFamilyBillingActivity.java
@@ -9,8 +9,17 @@ import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.RemoteException;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.amazon.device.iap.PurchasingListener;
+import com.amazon.device.iap.PurchasingService;
+import com.amazon.device.iap.model.ProductDataResponse;
+import com.amazon.device.iap.model.PurchaseResponse;
+import com.amazon.device.iap.model.PurchaseUpdatesResponse;
+import com.amazon.device.iap.model.UserDataResponse;
+import com.android.vending.billing.IInAppBillingService;
 import com.yellowforktech.littlefamilytree.data.DataService;
 import com.yellowforktech.littlefamilytree.db.FireHelper;
 
@@ -28,10 +37,17 @@ public class LittleFamilyBillingActivity extends LittleFamilyActivity {
 
     private ServiceConnection mServiceConn;
     private boolean connecting;
+    private boolean isAmazon = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        PackageManager pkgManager = this.getPackageManager();
+        String installerPackageName = pkgManager.getInstallerPackageName(this.getPackageName());
+        if(installerPackageName.startsWith("com.amazon")) {
+            isAmazon = true;
+        }
     }
 
     @Override
@@ -46,23 +62,33 @@ public class LittleFamilyBillingActivity extends LittleFamilyActivity {
 
     public void connectToStore() {
         connecting = true;
-        mServiceConn = new ServiceConnection() {
-            @Override
-            public void onServiceDisconnected(ComponentName name) {
-                mService = null;
-            }
 
-            @Override
-            public void onServiceConnected(ComponentName name,
-                                           IBinder service) {
-                mService = IInAppBillingService.Stub.asInterface(service);
-            }
-        };
+        if (isAmazon) {
 
-        Intent serviceIntent =
-                new Intent("com.android.vending.billing.InAppBillingService.BIND");
-        serviceIntent.setPackage("com.android.vending");
-        bindService(serviceIntent, mServiceConn, Context.BIND_AUTO_CREATE);
+            Log.d(this.getClass().getName().toString(), "onCreate: registering PurchasingListener");
+
+            PurchasingService.registerListener(this.getApplicationContext(), new LFTPurchasingListener());
+
+            Log.d(this.getClass().getName().toString(), "IS_SANDBOX_MODE:" + PurchasingService.IS_SANDBOX_MODE);
+        } else {
+            mServiceConn = new ServiceConnection() {
+                @Override
+                public void onServiceDisconnected(ComponentName name) {
+                    mService = null;
+                }
+
+                @Override
+                public void onServiceConnected(ComponentName name,
+                                               IBinder service) {
+                    mService = IInAppBillingService.Stub.asInterface(service);
+                }
+            };
+
+            Intent serviceIntent =
+                    new Intent("com.android.vending.billing.InAppBillingService.BIND");
+            serviceIntent.setPackage("com.android.vending");
+            bindService(serviceIntent, mServiceConn, Context.BIND_AUTO_CREATE);
+        }
     }
 
     @Override
@@ -79,13 +105,7 @@ public class LittleFamilyBillingActivity extends LittleFamilyActivity {
                     //alert("You have bought the " + sku + ". Excellent choice,
                     //        adventurer!");
                     if (sku != null && sku.equals(SKU_PREMIUM)) {
-                        this.hasPremium = true;
-                        DataService.getInstance().getDBHelper().saveProperty(LittleFamilyActivity.PROP_HAS_PREMIUM, "true");
-                        String username = DataService.getInstance().getDBHelper().getProperty(DataService.SERVICE_USERNAME);
-                        String serviceType = DataService.getInstance().getServiceType();
-                        if (username!=null && serviceType!=null) {
-                            FireHelper.getInstance().createOrUpdateUser(username, serviceType, true);
-                        }
+                        grantPremium();
                         return;
                     }
                 }
@@ -95,6 +115,21 @@ public class LittleFamilyBillingActivity extends LittleFamilyActivity {
                     finish();
                 }
             }
+            finish();
+        }
+    }
+
+    private void grantPremium() {
+        this.hasPremium = true;
+        try {
+            DataService.getInstance().getDBHelper().saveProperty(LittleFamilyActivity.PROP_HAS_PREMIUM, "true");
+            String username = DataService.getInstance().getDBHelper().getProperty(DataService.SERVICE_USERNAME);
+            String serviceType = DataService.getInstance().getServiceType();
+            if (username != null && serviceType != null) {
+                FireHelper.getInstance().createOrUpdateUser(username, serviceType, true);
+            }
+        } catch (Exception e) {
+            Log.e(this.getClass().getName().toString(), "Error reading products from store", e);
             finish();
         }
     }
@@ -109,24 +144,28 @@ public class LittleFamilyBillingActivity extends LittleFamilyActivity {
                 Bundle querySkus = new Bundle();
                 querySkus.putStringArrayList("ITEM_ID_LIST", skuList);
 
-                Bundle skuDetails = mService.getSkuDetails(3, getPackageName(), "inapp", querySkus);
+                try {
+                    Bundle skuDetails = mService.getSkuDetails(3, getPackageName(), "inapp", querySkus);
 
-                int response = skuDetails.getInt("RESPONSE_CODE");
-                if (response == 0) {
-                    ArrayList<String> responseList
-                            = skuDetails.getStringArrayList("DETAILS_LIST");
+                    int response = skuDetails.getInt("RESPONSE_CODE");
+                    if (response == 0) {
+                        ArrayList<String> responseList
+                                = skuDetails.getStringArrayList("DETAILS_LIST");
 
-                    for (String thisResponse : responseList) {
-                        try {
-                            JSONObject object = new JSONObject(thisResponse);
-                            String sku = object.getString("productId");
-                            String price = object.getString("price");
-                            //if (sku.equals("premiumUpgrade")) mPremiumUpgradePrice = price;
-                            //else if (sku.equals("gas")) mGasPrice = price;
-                        } catch (JSONException e) {
-                            Log.e(this.getClass().getName().toString(), "Error reading products from store", e);
+                        for (String thisResponse : responseList) {
+                            try {
+                                JSONObject object = new JSONObject(thisResponse);
+                                String sku = object.getString("productId");
+                                String price = object.getString("price");
+                                //if (sku.equals("premiumUpgrade")) mPremiumUpgradePrice = price;
+                                //else if (sku.equals("gas")) mGasPrice = price;
+                            } catch (JSONException e) {
+                                Log.e(this.getClass().getName().toString(), "Error reading products from store", e);
+                            }
                         }
                     }
+                } catch (RemoteException e) {
+                    Log.e(this.getClass().getName().toString(), "Error reading products from store", e);
                 }
             }
         };
@@ -136,31 +175,37 @@ public class LittleFamilyBillingActivity extends LittleFamilyActivity {
     }
 
     public void buyUpgrade() {
-        PackageManager pkgManager = this.getPackageManager();
-        String installerPackageName = pkgManager.getInstallerPackageName(this.getPackageName());
-        if(installerPackageName.startsWith("com.amazon")) {
+        if(isAmazon) {
             // Amazon
-
+            PurchasingService.purchase("LFTPremium");
         } else {
             // Google Play
             waitForConnect();
-            Bundle buyIntentBundle = mService.getBuyIntent(3, getPackageName(),
-                    SKU_PREMIUM, "inapp", "");
-            int response = buyIntentBundle.getInt("RESPONSE_CODE");
-            if (response == 0) {
-                PendingIntent pendingIntent = buyIntentBundle.getParcelable("BUY_INTENT");
-                try {
-                    startIntentSenderForResult(pendingIntent.getIntentSender(),
-                            1001, new Intent(), Integer.valueOf(0), Integer.valueOf(0),
-                            Integer.valueOf(0));
-                } catch (IntentSender.SendIntentException e) {
-                    Log.e(this.getClass().getName().toString(), "Error starting purchase intent", e);
+            try {
+                Bundle buyIntentBundle = mService.getBuyIntent(3, getPackageName(),
+                        SKU_PREMIUM, "inapp", "");
+                int response = buyIntentBundle.getInt("RESPONSE_CODE");
+                if (response == 0) {
+                    PendingIntent pendingIntent = buyIntentBundle.getParcelable("BUY_INTENT");
+                    try {
+                        startIntentSenderForResult(pendingIntent.getIntentSender(),
+                                1001, new Intent(), Integer.valueOf(0), Integer.valueOf(0),
+                                Integer.valueOf(0));
+                    } catch (IntentSender.SendIntentException e) {
+                        Log.e(this.getClass().getName().toString(), "Error starting purchase intent", e);
+                    }
                 }
+            } catch (RemoteException e) {
+                Log.e(this.getClass().getName().toString(), "Error purchasing upgrade from store", e);
             }
         }
     }
 
     protected void waitForConnect() {
+        if (isAmazon) {
+            connectToStore();
+            return;
+        }
         if (mService==null) {
             if (!connecting) {
                 connectToStore();
@@ -206,5 +251,36 @@ public class LittleFamilyBillingActivity extends LittleFamilyActivity {
                 });
             }
         });
+    }
+
+    class LFTPurchasingListener implements PurchasingListener {
+        @Override
+        public void onUserDataResponse(UserDataResponse userDataResponse) {
+
+        }
+
+        @Override
+        public void onProductDataResponse(ProductDataResponse productDataResponse) {
+
+        }
+
+        @Override
+        public void onPurchaseResponse(PurchaseResponse purchaseResponse) {
+            switch (purchaseResponse.getRequestStatus()) {
+                case ALREADY_PURCHASED:
+                case SUCCESSFUL:
+                    grantPremium();
+                    break;
+                default:
+                    finish();
+                    Toast.makeText(LittleFamilyBillingActivity.this, "Unable to complete purchase", Toast.LENGTH_LONG);
+                    break;
+            }
+        }
+
+        @Override
+        public void onPurchaseUpdatesResponse(PurchaseUpdatesResponse purchaseUpdatesResponse) {
+
+        }
     }
 }
